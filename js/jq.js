c556020c4e0b97f98b87672edefe95c073f2eff3
GuntarWi <guntarwi@gmail.com>
2015-06-17 20:22:05 +0300

    Add fix
    Need-to-re-build-fix

--- js/jq.js
+++ js/jq.js
@@ -5,28 +5,25 @@ $(document).ready(function(){
     $(".btn").click(function(){
         $(".footer-mail").addClass("hidden");
     });
-});
 
 
-$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
 
-    $("#owl-demo").owlCarousel({
-
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
+        //navigation : false, // Show next and prev buttons
+        //slideSpeed : 300,
+        //paginationSpeed : 400,
+        //singleItem:true
 
         // "singleItem:true" is a shortcut for:
-        // items : 1,
+         items : 1
         // itemsDesktop : false,
         // itemsDesktopSmall : false,
         // itemsTablet: false,
         // itemsMobile : false
 
     });
-
 });
+
 
 function initialize() {
     var mapProp = {
